1cea22b695fb8d513742ad5312f89ac1eccf014b
Omar-Abdullah-Shaikh-Al-Zoor <oas2713@rit.edu>
2022-07-31 07:25:20 +0400

    Update 1.1

--- CustomTkinter/CSVConvertor.py
+++ CustomTkinter/CSVConvertor.py
@@ -1,19 +1,13 @@
-import tkinter as tk
+import tkinter
 import tkinter.messagebox
 import customtkinter
-from tkinter import *
-from tkinter import filedialog
 from tkinter.filedialog import askopenfile
-
-from customtkinter import ThemeManager
-
 import Conv_func as cf
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 global csv_path, xml_path, paths
-
 
 class App(customtkinter.CTk):
     WIDTH = 780
@@ -26,31 +20,16 @@ class App(customtkinter.CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
-        # ============ create two frames ============
+        # ============ create a frame ============
 
         # configure grid layout (2x1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         self.frame_right = customtkinter.CTkFrame(master=self)
-        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+        self.frame_right.grid(row=0, column=1, sticky="n", padx=20, pady=20)
 
-        # ============ Appearance ============
-        self.label_mode = customtkinter.CTkLabel(master=self.frame_right,
-                                                 width=300,
-                                                 height=30, text_color="turquoise",
-                                                 text_font=("Arial", 15),
-                                                 text="Appearance Mode:")
-        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
 
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_right,
-                                                        text_color="turquoise",
-                                                        text_font=("Arial", 15),
-                                                        values=["Light", "Dark", "System"],
-                                                        width=320,
-                                                        height=40,
-                                                        command=self.change_appearance_mode)
-        self.optionmenu_1.grid(row=10, column=0, columnspan=1, rowspan=1, pady=5, padx=20, sticky="w")
 
         # ============ frame_right ============
 
@@ -70,7 +49,7 @@ class App(customtkinter.CTk):
         self.frame_info.columnconfigure(0, weight=1)
 
         self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
-                                                   text="CSV to XML Convertor",
+                                                   text="CSV to XML Convertor\n\n Credits: Omar Shaikh Abdullah Al Zoor",
                                                    text_color="turquoise",
                                                    text_font=("Arial", 15),
                                                    height=100,
@@ -79,47 +58,27 @@ class App(customtkinter.CTk):
                                                    justify=tkinter.CENTER)
         self.label_info_1.grid(column=0, row=0, sticky="nwe", pady=15, padx=20)
 
+        # ============ Buttons ============
+
         self.button_1 = customtkinter.CTkButton(master=self.frame_right,
-                                                width=300,
-                                                height=70,
+                                                width=370,
+                                                height=65,
                                                 text_color="turquoise",
                                                 text_font=("Arial", 15),
                                                 text="Choose a CSV File",
                                                 command=self.csv_upload)
-        self.button_1.grid(row=5, column=0, columnspan=1, rowspan=1, pady=10)
+        self.button_1.grid(row=5, column=0, columnspan=1, rowspan=1, padx=30,pady=10)
 
         self.button_2 = customtkinter.CTkButton(master=self.frame_right,
                                                 text="Choose the path of the new XML file",
-                                                width=300,
-                                                state=tkinter.NORMAL,
+                                                width=400,
+                                                state=tkinter.DISABLED,
                                                 text_color="turquoise",
                                                 text_font=("Arial", 13),
-                                                height=70,
+                                                height=65,
                                                 command=self.loc_xmlpath)
 
-        self.button_2.grid(row=6, column=0, columnspan=1, rowspan=1, pady=0, padx=50)
-
-        # ============ frame_right ============
-
-        self.radio_var = tkinter.IntVar(value=0)
-
-        self.label_radio_group = customtkinter.CTkLabel(master=self.frame_right,
-                                                        width=300,
-                                                        height=50,
-                                                        text_color="turquoise",
-                                                        text="Choose CSV to XML Converter mode",
-                                                        text_font=("Arial", 14))
-        self.label_radio_group.grid(row=5, column=1, columnspan=1, rowspan=1, pady=5, padx=0, sticky="")
-
-        self.combobox_1 = customtkinter.CTkComboBox(master=self.frame_right,
-                                                    width=100,
-                                                    height=50,
-                                                    text_color="turquoise",
-                                                    text_font=("Arial", 15),
-                                                    values=["XML Mode 1", "XML Mode 2"])
-
-        self.combobox_1.grid(row=6, column=1, columnspan=1, pady=20, padx=20, sticky="we")
-
+        self.button_2.grid(row=5, column=1, columnspan=1, rowspan=1, pady=10, padx=30)
         self.button_3 = customtkinter.CTkButton(master=self.frame_right,
                                                 text="Convert to XML",
                                                 width=300,
@@ -128,23 +87,70 @@ class App(customtkinter.CTk):
                                                 border_color="red",
                                                 border_width=3,  # <- custom border_width
                                                 fg_color=None,  # <- no fg_color
+                                                hover_color="tomato",
                                                 text_color="red",
                                                 state=tkinter.DISABLED,
                                                 text_font=("Arial", 20),
                                                 command=self.convertor)
         self.button_3.grid(row=9, column=1, columnspan=2, rowspan=2, pady=20, padx=20, sticky="we")
 
+        # ============ Radio Button and Combobox  ============
+
+        self.radio_var = tkinter.IntVar(value=0)
+
+        self.label_radio_group = customtkinter.CTkLabel(master=self.frame_right,
+                                                        width=300,
+                                                        height=60,
+                                                        text_color="turquoise",
+                                                        text="Choose XML Converter Mode",
+                                                        fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                        corner_radius=7,
+                                                        text_font=("Arial", 14),
+                                                       )
+        self.label_radio_group.grid(row=6, column=0, columnspan=1, rowspan=1, pady=5, padx=0, sticky="")
+
+        self.combobox_1 = customtkinter.CTkComboBox(master=self.frame_right,
+                                                    width=100,
+                                                    height=50,
+                                                    dropdown_text_color="turquoise",
+                                                    dropdown_text_font=("Arial", 12),
+                                                    text_color="turquoise",
+                                                    state=tkinter.DISABLED,
+                                                    text_font=("Arial", 15),
+                                                    command=self.xml_mode,
+                                                    values=["XML Mode 1", "XML Mode 2"])
+
+        self.combobox_1.grid(row=6, column=1, columnspan=1, pady=20, padx=20, sticky="we")
+
+        # ============ Appearance ============
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_right,
+                                                 width=300,
+                                                 height=30, text_color="blue",
+                                                 text_font=("Arial", 15),
+                                                 text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=50, sticky="w")
+
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_right,
+                                                        text_color="turquoise",
+                                                        text_font=("Arial", 15),
+                                                        dropdown_text_font=("Arial", 12),
+                                                        dropdown_text_color="turquoise",
+                                                        values=["Light", "Dark", "System"],
+                                                        width=320,
+                                                        height=40,
+                                                        command=self.change_appearance_mode)
+        self.optionmenu_1.grid(row=10, column=0, columnspan=1, rowspan=1, pady=5, padx=43, sticky="w")
+
         # set default values
         self.optionmenu_1.set("Dark")
-        self.combobox_1.set("XML Modes")
+
 
     def csv_upload(self):
         global csv_path, paths,filename
         csv_path, paths,filename = cf.csv_upload()
         self.label_info_1.set_text(paths)
         if paths != "none":
-            self.button_2.configure(state="tkinter.DISABLED")
-
+            self.button_2.configure(state=tkinter.NORMAL)
 
 
     def loc_xmlpath(self):
@@ -152,15 +158,28 @@ class App(customtkinter.CTk):
         xml_path, path = cf.loc_xmlpath(paths)
         self.label_info_1.set_text(path)
         if path != "none":
-            self.button_3.configure(state="tkinter.NORMAL")
+            self.combobox_1.configure(state=tkinter.NORMAL)
+            self.combobox_1.set("XML Modes")
 
+    def xml_mode(self,xml_value):
+
+        if self.combobox_1.current_value == "XML Mode 1" or self.combobox_1.current_value == "XML Mode 2":
+            self.button_3.configure(state=tkinter.NORMAL)
 
     def convertor(self):
-        value =cf.Convertor(csv_path, xml_path,filename)
-        if value==1:
-            self.label_info_1.set_text("Successfully Created XML File")
-        else:
-            self.label_info_1.set_text("Failed to Create XML File")
+        if self.combobox_1.values == "XML Mode 1":
+            value = cf.Convertor_m1(csv_path, xml_path,filename)
+            if value==1:
+                self.label_info_1.set_text("Successfully Created XML File")
+            else:
+                self.label_info_1.set_text("Failed to Create XML File")
+        elif self.combobox_1.values == "XML Mode 2":
+            value = cf.Convertor_m2(csv_path, xml_path, filename)
+            if value == 1:
+                self.label_info_1.set_text("Successfully Created XML File")
+            else:
+                self.label_info_1.set_text("Failed to Create XML File")
+
 
 
     def change_appearance_mode(self, new_appearance_mode):
--- CustomTkinter/Conv_func.py
+++ CustomTkinter/Conv_func.py
@@ -31,7 +31,7 @@ def loc_xmlpath(paths):
     return xml_path,path1
 
 
-def Convertor(csv_path, xml_path,filename):
+def Convertor_m1(csv_path, xml_path,filename):
     csvfile = csv_path
 
     def convert_row(headers, row):
@@ -51,4 +51,24 @@ def Convertor(csv_path, xml_path,filename):
     with open(xml_file, "w") as f:
         f.write(xml)
         return 1
+
+def Convertor_m2(csv_path, xml_path, filename):
+        csvfile = csv_path
+        def convert_row(headers, row):
+            s = f'<Contact">\n'
+            for header, item in zip(headers, row):
+                s += f'    <{header}>' + f'{item}' + f'</{header}>\n'
+            return s + '</Contact>'
+
+        with open(csvfile, 'r') as f:
+            r = csv.reader(f)
+            headers = next(r)
+            xml = '<PhoneBook>\n'
+            for row in r:
+                xml += convert_row(headers, row) + '\n'
+            xml += '</PhoneBook>'
+        xml_file = str(xml_path) + "/" + filename + ".xml"
+        with open(xml_file, "w") as f:
+            f.write(xml)
+            return 1
 
